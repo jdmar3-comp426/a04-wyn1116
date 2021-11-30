291eae8875dae4965dba33341a700bd68d95e04a
wyn1116 <wyn342@gmail.com>
2021-11-29 20:57:07 -0500

    Updated message for patch and delete

--- server.js
+++ server.js
@@ -46,14 +46,14 @@ app.get("/app/user/:id", (req, res) => {
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).send(info.changes+ " record updated: ID " +info.lastInsertRowid);
+	res.status(200).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (200)"});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const info = stmt.run(req.params.id);
-	res.status(200).send(info.changes+ " record updated: ID " +info.lastInsertRowid);
+	res.status(200).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (200)"});
 });
 
 // Default response for any other request
