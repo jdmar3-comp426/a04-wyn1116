ca48a1bda91e1af6dc25a51a3ed80458ebf8874f
wyn1116 <wyn342@gmail.com>
2021-11-29 20:48:12 -0500

    Misunderstanding of send command

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/user", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(201).json({"message":"1 record created: ID @id (201)"});
+	res.status(201).send(info.changes+ "record created: ID " +info.lastInsertRowid);
 });
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -46,14 +46,14 @@ app.get("/app/user/:id", (req, res) => {
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json({"message":"1 record updated: ID ? (200)"});
+	res.status(200).send(info.changes+ "record updated: ID" +info.lastInsertRowid);
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const info = stmt.run(req.params.id);
-	res.status(200).json({"message":"1 record deleted: ID @req.params.id (200)"});
+	res.status(200).send(info.changes+ "record updated: ID" +info.lastInsertRowid);
 });
 
 // Default response for any other request
